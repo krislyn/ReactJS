835b00e74ac29dd0fea2f2ed241691bfce0665af
Krislyn Luks <krsln@tlu.ee>
2019-12-10 12:22:43 +0200

    toit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Snake from './Snake';
+import Food from './Food';
 
 class App extends Component {
 
   state = {
+    food: [6,8],
     snakeDots: [
       [0,0],
       [2,0]
@@ -14,7 +16,7 @@ class App extends Component {
     return (
       <div className="game-area">
         <Snake snakeDots={this.state.snakeDots}/>
-
+        <Food fot={this.state.food}/>
       </div>
     
     );
